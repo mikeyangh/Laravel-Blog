19f62b4e3196ef552cd17347db850cb17e69c21f
mikeyangh <472457791@qq.com>
2017-04-14 01:56:53 -0700

    Manipulate comments

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,7 +33,11 @@ Route::group(['middlewareGroups' => ['web']], function(){
     Route::resource('tags', 'TagController', ['except' => ['create']]);
 
     // Comments
-    Route::post('comments{post_id}', ['as' => 'comments.store', 'uses' => 'CommentsController@store']);
+    Route::post('comments/{post_id}', ['as' => 'comments.store', 'uses' => 'CommentsController@store']);
+    Route::get('comments/{id}/edit', ['as' => 'comments.edit', 'uses' => 'CommentsController@edit']);
+    Route::put('comments/{id}', ['as' => 'comments.update', 'uses' => 'CommentsController@update']);
+    Route::delete('comments/{id}', ['as' => 'comments.destroy', 'uses' => 'CommentsController@destroy']);
+    Route::get('comments/{id}/delete', ['as' => 'comments.delete', 'uses' => 'CommentsController@delete']);
 
     Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])
     ->where('slug', '[\w\d\-\_]+');
